e5224715bd754a3c918ebe96b9421e7840f4288a
Aram <aramhrptn@hotmail.com>
2025-07-03 04:12:37 +0400

    Add DeleteCustomAddons function to uninstall all custom addons from the cluster

--- pkg/addons/custom.go
+++ pkg/addons/custom.go
@@ -26,6 +26,23 @@ func ApplyCustomAddons(cluster *types.Cluster, logger *utils.Logger) {
 	}
 }
 
+// DeleteCustomAddons uninstalls all custom addons (manifest or Helm) for the given cluster.
+//
+// Parameters:
+//
+//	cluster: The cluster to uninstall custom addons from.
+//	logger: Logger for output.
+func DeleteCustomAddons(cluster *types.Cluster, logger *utils.Logger) {
+	for name, addon := range cluster.CustomAddons {
+		if addon.Manifest != nil {
+			deleteCustomManifestAddon(name, cluster, addon.Manifest, logger)
+		}
+		if addon.Helm != nil {
+			deleteCustomHelmAddon(name, cluster, addon.Helm, logger)
+		}
+	}
+}
+
 func applyCustomManifestAddon(name string, cluster *types.Cluster, manifest *types.ManifestConfig, logger *utils.Logger) {
 	kubeconfig := clusterutils.KubeConfigPath(cluster, logger)
 	manifestPath := manifest.Path
@@ -36,6 +53,18 @@ func applyCustomManifestAddon(name string, cluster *types.Cluster, manifest *typ
 	}
 	logger.Log("Applying custom manifest addon '%s' from %s", name, manifestPath)
 	clusterutils.ApplyComponentYAML(name, kubeconfig, manifestPath, logger, subs)
+}
+
+func deleteCustomManifestAddon(name string, cluster *types.Cluster, manifest *types.ManifestConfig, logger *utils.Logger) {
+	kubeconfig := clusterutils.KubeConfigPath(cluster, logger)
+	manifestPath := manifest.Path
+	subs := manifest.Subs
+	if manifestPath == "" {
+		logger.Log("Custom manifest addon '%s' missing path", name)
+		return
+	}
+	logger.Log("Deleting custom manifest addon '%s' from %s", name, manifestPath)
+	clusterutils.DeleteComponentYAML(name, kubeconfig, manifestPath, logger, subs)
 }
 
 func applyCustomHelmAddon(name string, cluster *types.Cluster, helm *types.HelmConfig, logger *utils.Logger) {
@@ -62,5 +91,21 @@ func applyCustomHelmAddon(name string, cluster *types.Cluster, helm *types.HelmC
 	)
 	if err != nil {
 		logger.LogErr("Helm install failed for custom addon '%s': %v", name, err)
+	}
+}
+
+func deleteCustomHelmAddon(name string, cluster *types.Cluster, helm *types.HelmConfig, logger *utils.Logger) {
+	kubeconfig := clusterutils.KubeConfigPath(cluster, logger)
+	if helm.Chart == "" || helm.Repo.URL == "" {
+		logger.Log("Custom Helm addon '%s' missing chart or repo URL", name)
+		return
+	}
+	namespace := helm.Namespace
+	if namespace == "" {
+		namespace = "default"
+	}
+	logger.Log("Uninstalling custom Helm addon '%s' (chart: %s, repo: %s)", name, helm.Chart, helm.Repo.URL)
+	if err := clusterutils.UninstallHelmRelease(kubeconfig, name, namespace, logger); err != nil {
+		logger.LogErr("Helm uninstall failed for custom addon '%s': %v", name, err)
 	}
 }
